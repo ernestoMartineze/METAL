68cc97d9f3488421c18a8f19890b739cb97a8383
ernestoMartineze <ernesto.martinez@soultech.com.mx>
2018-12-03 19:08:12 -0600

    gestor edo cuenta

--- frisa-servicios/frisa-servicios-ejb/src/main/java/mx/frisa/tic/negocio/ingresos/GestorEstadoCuenta.java
+++ frisa-servicios/frisa-servicios-ejb/src/main/java/mx/frisa/tic/negocio/ingresos/GestorEstadoCuenta.java
@@ -6,13 +6,16 @@
 package mx.frisa.tic.negocio.ingresos;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import mx.frisa.tic.datos.comun.DAO;
+import mx.frisa.tic.datos.dto.comun.CatalogoParametroDTO;
+import mx.frisa.tic.datos.dto.comun.ParametroDTO;
 import mx.frisa.tic.datos.dto.ingresos.RespuestaDTO;
+import mx.frisa.tic.datos.entidades.XxfrLineaCaptura;
 import mx.frisa.tic.datos.entidades.XxfrtEstadoCuenta;
 import mx.frisa.tic.datos.enums.ProcesoEnum;
 import mx.frisa.tic.negocio.remoto.AdaptadorWS;
@@ -40,18 +43,19 @@ public class GestorEstadoCuenta implements GestorEstadoCuentaLocal {
             respuesta.setDescripcionError(respuestaWS.getProceso().getDescripcion());
             if (respuestaWS.getProceso().getTermino().equals("0")) {
                 //Proceso exitoso persistir en BD estados de cuenta
-                List<G_1> lineasPago= respuestaWS.getDATA_DSObject().getG_1();
+                List<G_1> lineasPago = respuestaWS.getDATA_DSObject().getG_1();
+                DAO<XxfrLineaCaptura> LineaCapturaDao = new DAO(XxfrLineaCaptura.class);
                 DAO<XxfrtEstadoCuenta> estadoCuentaDao = new DAO(XxfrtEstadoCuenta.class);
-                for (G_1 lineaPago : lineasPago){
-                    
+                for (G_1 lineaPago : lineasPago) {
+
                     manejaLog.debug("Procesando el estado de cuenta : getBANK_ACCOUNT_NUM" + lineaPago.getBANK_ACCOUNT_NUM());
                     manejaLog.debug("getSTMT_TO_DATE : " + lineaPago.getSTMT_TO_DATE());
                     manejaLog.debug("getLINE_NUMBER : " + lineaPago.getLINE_NUMBER());
-                    
+
                     XxfrtEstadoCuenta edoCuenta = new XxfrtEstadoCuenta();
                     edoCuenta.setBankAccountNum(BigDecimal.valueOf(Long.valueOf(lineaPago.getBANK_ACCOUNT_NUM())));
-                    edoCuenta.setAddiotionalEntryInformation(lineaPago.getAMOUNT());
-                    edoCuenta.setTrxDate(new Date());
+//                    edoCuenta.setTrxDate(new Date());
+                    edoCuenta.setIdEdoCta(null);
                     edoCuenta.setLineNumber(BigDecimal.valueOf(Long.valueOf(lineaPago.getLINE_NUMBER())));
                     edoCuenta.setTrxType(lineaPago.getTRX_TYPE());
                     edoCuenta.setAmount(BigDecimal.valueOf(Long.valueOf(lineaPago.getAMOUNT())));
@@ -60,15 +64,28 @@ public class GestorEstadoCuenta implements GestorEstadoCuentaLocal {
                     edoCuenta.setCustomerReference("123456789012341"); //CUSTOMER_REFERENCE
                     edoCuenta.setAddiotionalEntryInformation("WERTYUTR|011510000019400000026417676205|DFGSDFGHFDGHERYRSGSFRG|123456789012341|RWTEYUHGFDS#$"); //ADDIOTIONAL_ENTRY_INFORMATION
                     edoCuenta.setProyectoPropietario("151"); //PROYECTO_PROPIETARIO
-                    edoCuenta.setLineCapture("011510000019400000026417676205");  //Linea captura
-                    
-                    //Guardar en base de datos el estado de cuenta
-                    estadoCuentaDao.registra(edoCuenta);
-                    if (!estadoCuentaDao.getProceso().getTermino().equals("0")){
-                        manejaLog.debug("Error al procesar el estado de cuenta : " + estadoCuentaDao.getProceso().getDescripcion() + ", NoLinea : " + lineaPago.getLINE_NUMBER());
+                    String lineaCapturaEnERP = "011510000019400000026417676205";
+                    edoCuenta.setLineCapture(lineaCapturaEnERP);  //Linea captura
+                    List<CatalogoParametroDTO> plistaParametros = new ArrayList<>();
+                    plistaParametros.add (new CatalogoParametroDTO("lineacaptura",lineaCapturaEnERP,"CADENA"));
+                    List<XxfrLineaCaptura> lineasCaptura = (List<XxfrLineaCaptura>) LineaCapturaDao.consultaQueryByParameters("XxfrLineaCaptura.findByLineacaptura", plistaParametros);
+                    if (!estadoCuentaDao.getProceso().getTermino().equals("0")) {
+                        edoCuenta.setIdLineaCaptura(lineasCaptura.get(0).getIdlineacaptura());  // ID Linea captura
                     }
+                    //Guardar en base de datos el estado de cuenta si es valida la linea de captura y su monto
+                    if (lineasCaptura.get(0).getMontolineacaptura() == BigDecimal.valueOf(Long.valueOf(lineaPago.getAMOUNT()))) {
+                        estadoCuentaDao.actualizaQuery("INSERT INTO \"INGRESOS\".\"XXFR_ESTADO_CUENTA\" (BANK_ACCOUNT_NUM, TRX_DATE, LINE_NUMBER, TRX_TYPE, AMOUNT, TRX_CODE, CURRENCY_CODE, CUSTOMER_REFERENCE, ADDIOTIONAL_ENTRY_INFORMATION, PROYECTO_PROPIETARIO, LINE_CAPTURE) VALUES ('1136556', TO_DATE('2018-12-04 00:01:00', 'YYYY-MM-DD HH24:MI:SS'), '1', 'qweqe', '123', '12', 'mfg', '2312312', 'wasdfsadf23', '151', 'wesdgfhdsa')");
+                        if (!estadoCuentaDao.getProceso().getTermino().equals("0")) {
+                            manejaLog.debug("Error al procesar el estado de cuenta : " + estadoCuentaDao.getProceso().getDescripcion() + ", NoLinea : " + lineaPago.getLINE_NUMBER());
+                            respuesta.setProceso(ProcesoEnum.ERROR.toString());
+                        } else {
+                            respuesta.setProceso(ProcesoEnum.EXITOSO.toString());
+                        }
+                    } else {
+                        respuesta.setProceso(ProcesoEnum.ERROR.toString());
+                    }
+
                 }
-                respuesta.setProceso(ProcesoEnum.EXITOSO.toString());
 
             } else {
                 //Notificar error detectado
