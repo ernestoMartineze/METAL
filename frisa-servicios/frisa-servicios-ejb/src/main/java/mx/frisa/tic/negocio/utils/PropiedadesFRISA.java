8ceb823cee8c09050678984b3d44441306c920ac
DESKTOP-NCKTN08\soultech <ernesto.martinez@soultech.com.mx>
2018-12-05 11:39:31 -0600

    properties para WS ERP

---
+++ frisa-servicios/frisa-servicios-ejb/src/main/java/mx/frisa/tic/negocio/utils/PropiedadesFRISA.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.frisa.tic.negocio.utils;
+
+/**
+ *
+ * @author soultech
+ */
+public class PropiedadesFRISA {
+    
+}
