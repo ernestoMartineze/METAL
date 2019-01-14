b7018ba380938778e5a18a5b83ee1d43d5448780
noe hernandez <noe_5991@hotmail.com>
2019-01-14 12:26:10 -0600

    agrego bean para pagos sin referencia
    agrego bean para pagos sin referencia

---
+++ Frisa-Consultas/ViewController/src/mx/frisa/tic/view/backing/PagoSinRefernciaBean.java
@@ -0,0 +1,14 @@
+package mx.frisa.tic.view.backing;
+
+import java.io.Serializable;
+
+public class PagoSinRefernciaBean implements Serializable {
+    public PagoSinRefernciaBean() {
+    }
+
+    public String b1_action() {
+        // Add event code here...
+        System.out.println("press button");
+        return null;
+    }
+}
